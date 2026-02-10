b9acceb84b5842f7be50ea1814473d1c602f1e10
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-10 16:20:32 +0530

    use of useRouter hook

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/router";
 
 const EventPage = () => {
-  return <div>EventPage</div>;
+  const router = useRouter();
+  return (
+    <>
+      <h1>EventPage</h1>
+      <h3>{router.query.slug}</h3>
+    </>
+  );
 };
 
 export default EventPage;
