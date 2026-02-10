dd2c7db419500da7bb249c1e850bd56d775ca65f
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-10 16:15:51 +0530

    used Link from next/link in about page for navigation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <h1>about</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
 };
 
 export default about;
