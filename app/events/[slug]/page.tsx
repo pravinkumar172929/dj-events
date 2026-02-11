ab2a0a7730e55e34fd3eb4e423db72e4b691acbb
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-11 13:26:28 +0530

    added meta tag and title in head from next

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/router";
 
 const EventPage = () => {
-  const router = useRouter();
   return (
     <>
       <h1>EventPage</h1>
-      <h3>{router.query.slug}</h3>
     </>
   );
 };
